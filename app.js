7808100f30331afc7f1926256c155b15df128be1
justin woodard <jwoodard2092@gmail.com>
2022-02-18 13:48:23 -0600

    JS-Promises

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let watching = new Promise((resolve, reject) => {
+    let userWatchingLiveStream;
+});
+
+if (userWatchingLiveStream == true){
+    resolve("Thumbs up and Subscribe!")
+}else{
+    reject("User Left.")
+}
+
+watching.then((message) => {
+    console.log(message)
+}).catch((error) =>{
+    console.log(error)
+})
